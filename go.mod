b8e49dc18af86e87f9f1cdc11656cd94cc110127
Christian Mc Carthy <cmccarthy@pivotal.io>
2019-05-08 17:02:51 +0100

    foo
    Signed-off-by: Katie Chapman <kchapman@pivotal.io>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cmccarthy101/test11
+
+require github.com/cmccarthy101/test10 v0.0.0-20190508160027-f70a194aee79 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cmccarthy101/test10/demo"
+)
+
+func main() {
+	fmt.Println(demo.Greeting)
+}
